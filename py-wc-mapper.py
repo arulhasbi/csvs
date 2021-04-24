5c76c239fa88b96bf22146896a564343faae4542
arulhasbi <hasbi.fahrul@gmail.com>
2021-04-24 13:16:17 +0800

    Add files via upload

---
+++ py-wc-mapper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+
+for line in sys.stdin:
+
+    line = line.strip()
+
+    words = line.split()
+
+    for word in words:
+
+        print("{}\t{}".format(word, 1))
